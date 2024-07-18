bdf4deedb9dd67be6104515ed2094310d98651f2
Rail Khabirov <rail201@yandex.ru>
2024-07-18 15:26:45 +0300

    Add GenericRepository

---
+++ Domain/Interfaces/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        T Get(int id);
+        IEnumerable<T> GetAll();
+
+        IEnumerable<T> Finde(Expression<Func<T, bool>> expression);
+
+        void Add (T item);
+
+        void Remove (T item);
+    }
+}
