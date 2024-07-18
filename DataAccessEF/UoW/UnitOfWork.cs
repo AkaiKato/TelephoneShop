823e00599479a6fa749d8df40face198247087ef
Rail Khabirov <rail201@yandex.ru>
2024-07-18 17:39:17 +0300

    fix UoW

--- DataAccessEF/UoW/UnitOfWork.cs
+++ DataAccessEF/UoW/UnitOfWork.cs
@@ -1,11 +1,7 @@
 ﻿using DataAccessEF.Data;
+using DataAccessEF.TypeRepository;
 using Domain.Interfaces;
 using Domain.Interfaces.UoW;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataAccessEF.UoW
 {
@@ -13,18 +9,22 @@ namespace DataAccessEF.UoW
     {
         private readonly DataContext _context;
 
+        public ICatalogRepository CatalogRepository { get; private set; }
+
+        public ICitiesRepository CitiesRepository { get; private set; }
+
+        public ICitiesToTelephoneCostRepository CitiesToTelephoneCost { get; private set; }
+
+        public ITelephoneRepository TelephoneRepository { get; private set; }
+
         public UnitOfWork(DataContext context)
         {
             _context = context;
+            CatalogRepository = new CatalogRepository(_context);
+            CitiesRepository = new CitiesRepository(_context);
+            CitiesToTelephoneCost = new CitiesToTelephoneCostRepository(_context);
+            TelephoneRepository = new TelephoneRepository(_context);
         }
-
-        public ICatalogRepository CatalogRepository { get; private set; }
-
-        public ICitiesRepository CitiesRepository {get; private set; }
-
-        public ICitiesToTelephoneCostRepository CitiesToTelephoneCost {get; private set; }
-
-        public ITelephoneRepository TelephoneRepository { get; private set; }
 
         public void Dispose()
         {
---
+++ TelephoneShop/Controllers/CitiesController.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Interfaces.UoW;
+using Microsoft.AspNetCore.Mvc;
+using TelephoneShop.Models;
+
+namespace TelephoneShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CitiesController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IEnumerable<Cities> GetAll()
+        {
+            return _unitOfWork.CitiesRepository.GetAll();
+        }
+    }
+}
